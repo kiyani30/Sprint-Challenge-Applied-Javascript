823892b7a20892262cd19631ac12f41f70b0e534
kiyani <bamba.kiyani@gmail.com>
2019-09-06 03:36:36 -0400

    Completed Header components

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+    
+    const container = document.createElement("div");
+    const date = document.createElement("span");
+    const title = document.createElement("h1");
+    const temp = document.createElement("span");
+
+
+    date.textContent = "SMARCH 28, 2019";
+    title.textContent = "Lambda Times";
+    temp.textContent = "98°";
+  
+    container.appendChild(date);
+    container.appendChild(title);
+    container.appendChild(temp);
+  
+    container.classList.add("header");
+    date.classList.add("date");
+    temp.classList.add("temp");
+  
+  
+    return container;
+  }
+  
+  const newheader = Header();
+  const container = document.querySelector(".header-container");
+  container.appendChild(newheader);
