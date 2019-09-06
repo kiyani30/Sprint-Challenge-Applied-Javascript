6e66cfcaeef27b7540bf557dafde1c4b1176a372
kiyani <bamba.kiyani@gmail.com>
2019-09-06 05:02:23 -0400

    Created function CardMaker for Cards component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,41 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardContainer = document.querySelector(".cards-container");
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+       articles = response.data.articles;
+       articles.forEach(CardContent => {
+           const newArticle = CardMaker(CardContent);
+           cardContainer.appendChild(newArticle);    
+       });
+    })
+
+function CardMaker (CardContent){
+    
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const authorsName = document.createElement('span');
+
+    card.classList.add(".card");
+    headline.classList.add(".headline");
+    author.classList.add(".author");
+    imgContainer.classList.add(".img-container");
+    image.classList.add("img");
+    authorsName.classList.add("span");
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    card.appendChild(imgContainer);
+    card.appendChild(image);
+    card.appendChild(authorsName);
+
+    card.textContent = CardContent ;
+
+    return card;
+}
