878a56ee40afa0605dda67b952f8de6c42b878a8
kiyani <bamba.kiyani@gmail.com>
2019-09-06 04:13:55 -0400

    Completed tabs components

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,43 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+// axios.get('https://lambda-times-backend.herokuapp.com/topics')
+//     .then(response => {
+//     response.data.topics.forEach(topic => {
+//         tabs.appendChild(tabMaker(topic));
+//         const tabs = document.querySelector('.topic');
+
+//     })
+// })
+
+// function tabMaker(tab){
+//     const tabdiv = document.createElement('div');
+//     tabdiv.classList.add('tab');
+//     tabdiv.textcontent = tab;
+
+//     return tabdiv;
+// }
+
+const topicsContainer = document.querySelector(".topics");
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+        topics = response.data.topics;
+        topics.forEach(topic => {
+        const newTab = tabMaker(topic);
+        topicsContainer.appendChild(newTab);
+        });
+ })
+    .catch(response => {
+    console.log("Error message for 'Tabs' API call");
+    });
+    
+ function tabMaker(topic) {
+ const tabs = document.createElement('div');
+ tabs.classList.add('tab');
+ tabs.textContent = topic;
+
+ return tabs;
+ }
